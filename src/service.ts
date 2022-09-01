2c0a3f488e9893e24f880fcf632472f51132edb8
Alexandra Khutorna <a.khutorna@izzi.software>
2022-09-01 18:24:26 +0300

    test

--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,13 @@
-import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
 
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  //...
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCVB-d1UX3NbjuQ0uzrbj2hg7tLIkdFrj4',
+//   authDomain: 'family-tree-dccab.firebaseapp.com',
+//   projectId: 'family-tree-dccab',
+//   storageBucket: 'family-tree-dccab.appspot.com',
+//   messagingSenderId: '640000077296',
+//   appId: '1:640000077296:web:012df715adaabefcbdc135',
+// };
 
-export const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
