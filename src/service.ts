14c4c0b24736332a1b4da689d93c1a93ca9f4d51
Alexandra Khutorna <a.khutorna@izzi.software>
2022-09-02 13:48:34 +0300

    test

--- src/service.ts
+++ src/service.ts
@@ -1,13 +1,13 @@
-// import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCVB-d1UX3NbjuQ0uzrbj2hg7tLIkdFrj4',
-//   authDomain: 'family-tree-dccab.firebaseapp.com',
-//   projectId: 'family-tree-dccab',
-//   storageBucket: 'family-tree-dccab.appspot.com',
-//   messagingSenderId: '640000077296',
-//   appId: '1:640000077296:web:012df715adaabefcbdc135',
-// };
+const firebaseConfig = {
+  apiKey: 'AIzaSyCVB-d1UX3NbjuQ0uzrbj2hg7tLIkdFrj4',
+  authDomain: 'family-tree-dccab.firebaseapp.com',
+  projectId: 'family-tree-dccab',
+  storageBucket: 'family-tree-dccab.appspot.com',
+  messagingSenderId: '640000077296',
+  appId: '1:640000077296:web:012df715adaabefcbdc135',
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
